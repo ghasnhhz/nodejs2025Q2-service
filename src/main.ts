883c8214c858be15102e6a4b2d0472c59aa96318
ghasnhhz <shahzodkarimov1001@gmail.com>
2025-12-01 01:48:56 +0500

    feat: implement user path

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('Port') || 4000;
+  await app.listen(port);
 }
 bootstrap();
